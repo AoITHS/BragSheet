73fbea81e2c6da8d4e0f7fdbf26508190dc4e1e8
arehman0321 <arehman@aoiths.org>
2020-01-07 17:08:13 -0500

    stuff

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ app.get('/', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render("login");
-    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render("register");
 });
 
 app.listen(port, () => {
