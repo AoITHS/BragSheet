19ba0ff08c2d463a6227713ab8bca78634a0a225
arehman0321 <arehman@aoiths.org>
2020-01-14 17:20:53 -0500

    stuff

---
+++ public/js/register.js
@@ -0,0 +1,15 @@
+var email,
+    osis,
+    pass,
+    cpass;
+
+document.querySelector(".btn").addEventListener("click", () =>{
+    email = document.querySelector(".email").value.trim();
+    osis = document.querySelector(".osis").value.trim();
+    pass = document.querySelector(".pass").value.trim();
+    cpass = document.querySelector(".cpass").value.trim();
+    
+    if( (email === "") || (osis === "") || (pass === "") || (cpass === "") ){
+        document.querySelector(".error").innerHTML = `something is blank`;
+    }
+});
