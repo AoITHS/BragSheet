f38ac9e69b10cac96871fb859358c3c043881732
arehman0321 <arehman@aoiths.org>
2020-03-06 10:49:13 -0500

    stuff

--- public/js/form.js
+++ public/js/form.js
@@ -12,18 +12,38 @@ var partOne = {
     gender: null,
     race: null,
     languages: null     
-};
+},
+    Gvalid = false,
+    Rvalid = false;
 
 var gen = document.getElementsByName('gender');
 
 gen.forEach( (v, i) => {
     gen[i].onchange = function (){
-        if( v.value === 'other' ) d('.g-other-v').classList.remove('d-none');
-        else {
-            d('.g-other-v').classList.add('d-none');
-            partOne.gender = v.value;
-            console.log(partOne);
+        if( v.value === 'other' ) 
+        {
+            Gvalid = true;
+            d('.g-other-v').classList.remove('d-none');
         }
+        else 
+        {
+            Gvalid = false;
+            d('.g-other-v').classList.add('d-none');
+            
+        }
+    }
+});
+
+
+d('#r-other').addEventListener('change', () => {
+    if( d('#r-other').checked ) 
+    {
+        d('.r-other-v').classList.remove('d-none');
+        
+    }
+    else 
+    {
+        d('.r-other-v').classList.add('d-none');
     }
 });
 
@@ -34,7 +54,19 @@ d('.part-one').addEventListener('click', () => {
     partOne.middileInitial = d('.mi').value;
     partOne.email = d('.email').value;
     partOne.dob = d('.dob').value;
-
+    partOne.languages = d('.lang').value;
     
+    if( Gvalid === true )
+    {
+        var Gv = d('.g-other-v').value.trim();
+        if( Gv === '' )
+        {
+            alert(0);
+        }
+    }
+    else
+    {
+        alert(1);
+    }
 
 });
